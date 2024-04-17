f2dd9900366e7093ae36f55fe907fddfcfa6ecb7
Valdir Mendes <valdir.mfjesus@hotmail.com>
2024-04-17 00:23:55 -0300

    chore: configuring unit tests

---
+++ src/lib/probability_test.mjs
@@ -0,0 +1,8 @@
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+
+describe('K-sided probability suite tests', async () => {
+    it('first test', async () => {
+        assert.equal(1,1)
+    });
+});
